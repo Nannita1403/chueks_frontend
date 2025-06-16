02fe31dab99ff81fc794e6648e882fb83e80859e
Nannita1403 <nannimagi@gmail.com>
2025-06-16 07:55:54 +0200

    change front

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css"
+import { Button, HStack } from "@chakra-ui/react"
+
 
 const App = () => {
   return (
     <div>App</div>
   )
 }
+const Demo = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  )
+}
 
-export default App
+export default  App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
-
+import {Provider} from "../src/components/ui/provider.jsx"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ChakraProvider } from "@chakra-ui/react"
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(<Provider>
+    <App />
+    </Provider>)
