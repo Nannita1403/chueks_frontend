7ea3a7e1e24f453cc5cd958148c3c25d94db8eb0
Nannita1403 <nannimagi@gmail.com>
2025-06-22 23:28:27 +0200

    change in register infoPanel

--- src/components/LoginRegister/InfoPanel.jsx
+++ src/components/LoginRegister/InfoPanel.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, VStack, Text, Heading,  } from '@chakra-ui/react';
+import { Box, VStack, Text, Heading, Flex,  } from '@chakra-ui/react';
 import { useColorModeValue } from '../ui/color-mode';
 
 const InfoPanel = () => {
@@ -9,17 +9,37 @@ const InfoPanel = () => {
   return (
     <Box flex="1" bg={bg} color={color} p={10} display="flex" alignItems="center" justifyContent="center">
       <VStack spacing={4} textAlign="center">
-        <Heading size="2xl" textAlign="center">Catálogo Exclusivo de Accesorios y Carteras</Heading>
-        <Box>
-        <Text fontSize="md">
-          Inicia sesión o crea una cuenta para comenzar a explorar nuestros productos exclusivos y acceder a tus favoritos.
-        </Text>
-        </Box>
-        <Box>
-        <Text fontSize="sm" color="gray.500">
-          ¡Te esperamos con muchas sorpresas!
-        </Text>
-        </Box>
+        <Heading size="3xl" textAlign="center">Catálogo Exclusivo de Accesorios y Carteras</Heading>
+        <Text fontSize="xl" semibold mr={2}>
+          Bienvenido a CHUEKS, tu destino para accesorios y carteras de diseño exclusivo para venta mayorista.
+        </Text>     
+    <Box>
+      <Flex align="left" mb={1}>
+        <Box  w="1rem" h="1rem" bg="pink.500" borderRadius="full" mr={2} semibold/>
+        <Heading as="h3" size="2xl"> Diseños Exclusivos </Heading>
+      </Flex>
+      <Text color="gray.300" pl={6} size="xl">
+        Accede a nuestra colección de productos únicos y de alta calidad.
+      </Text>
+    </Box>
+       <Box>
+      <Flex align="left" mb={1}>
+        <Box  w="1rem" h="1rem" bg="cyan.400" borderRadius="full" mr={2} semibold/>
+        <Heading as="h3" size="2xl"> Información Detallada </Heading>
+      </Flex>
+      <Text color="gray.300" pl={6} size="xl">
+        Consulta precios, colores, categorias y materiales de confección.
+      </Text>
+    </Box>
+       <Box>
+      <Flex align="left" mb={1}>
+        <Box  w="1rem" h="1rem" bg="yellow.400" borderRadius="full" mr={2} semibold/>
+        <Heading as="h3" size="2xl"> Listas Personalizadas</Heading>
+      </Flex>
+      <Text color="gray.300" pl={6} size="xl">
+      Guarda tus favoritos y crea listas de compra para enviar al administrador
+      </Text>
+    </Box>
       </VStack>
     </Box>
   );
