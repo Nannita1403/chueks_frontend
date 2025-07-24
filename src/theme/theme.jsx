7c568d155b9220bc7ffb5b8a1ceabcd2e5bf3b64
Natalia <nannimagi@gmail.com>
2025-07-24 18:52:59 +0200

    change teem

--- src/theme/theme.jsx
+++ src/theme/theme.jsx
@@ -3,18 +3,25 @@ import { extendTheme } from "@chakra-ui/react";
 export const themeSystem = extendTheme({
   colors: {
     brand: {
-      0:"#ff0080",// Color magenta
-      1:"#00e6e6",// Color cyan
-      2: "#ffff00",// Color amarillo
-      3: "#7cfc00",// Color verde
-      4: "#9370db",// Color violeta
-      50: "#ffffff", //Color Blanco
-      100: "#f6f6f6",// Color gris claro bg      
-      200: "#e2e2e2",// Color gris claro bgButton-border
-      300: "#9d9d9d",// Color gris oscuro letra-borderselected
-      400: "#595959",// Color gris oscuro bgFondo 
-      500:"#000000",// Color negro
+     brand: {
+      50: "#fdf2f8",
+      100: "#fce7f3",
+      200: "#fbcfe8",
+      300: "#f9a8d4",
+      400: "#f472b6",
+      500: "#ec4899", // fuchsia-500
+      600: "#db2777", // fuchsia-600
+      700: "#be185d", // fuchsia-700
+      800: "#9d174d", // fuchsia-800
+      900: "#831843",
     },
+    cyan: {
+      500: "#0BC5EA",
+    },
+    yellow: {
+      400: "#F6E05E",
+    },
+  },
     background: "#f7fafc",
     text: "#1a202c",
     muted: "#718096",
