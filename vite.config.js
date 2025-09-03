fb0a1536314df68348a3061fc4712e9bc068c4d1
Natalia <nannimagi@gmail.com>
2025-09-03 17:36:05 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 
-import { defineConfig } from "vite"
+/*import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import path from "path"
 
@@ -13,4 +13,11 @@ export default defineConfig({
   server: {
     port: 5000,
   },
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
