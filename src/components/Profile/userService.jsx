c52652219959798faca197c27f2b956482638410
Natalia <nannimagi@gmail.com>
2025-11-06 13:17:00 +0100

    -

--- src/components/Profile/userService.jsx
+++ src/components/Profile/userService.jsx
@@ -1,4 +1,4 @@
-import ApiService from "../reducers/api/Api.jsx";
+import ApiService from "../../reducers/api/Api.jsx";
 
 export const editUserField = async (field, action, data = {}, id = null) => {
   return ApiService.patch("/users/edit", {
